52775be09590483230e225d5a32d651df2f5a871
Tom Marshall <tdm.code@gmail.com>
2013-02-07 14:05:53 -0800

    Add support for fetching Bluetooth MAC on i_vzw.

--- hwaddrs/getmac.c
+++ hwaddrs/getmac.c
@@ -27,20 +27,59 @@ extern void oncrpc_init();
 extern void oncrpc_task_start();
 extern void nv_cmd_remote(int, int, void*);
 
+static int is_macbuf_valid(const unsigned char *macbuf)
+{
+	return (macbuf[0] != 0 || macbuf[1] != 0 || macbuf[2] != 0);
+}
+
+/*
+ * Get the Bluetooth MAC address.
+ *
+ * The MAC may be found at one of several places, depending on the phone
+ * model and the messages that rild processes.  Try the following places,
+ * in the order listed:
+ *
+ *   - i_atnt and i_skt rild set system property service.brcm.bt.mac after
+ *     processing GET_IMEI/GET_IMEINV.  This seems reliable.
+ *
+ *   - i_vzw rild sets system property persist.service.brcm.bt.mac after
+ *     processing some unknown sequence of (possibly proprietary) messages.
+ *     So far, this has only been observed with the vendor framework.
+ *
+ *   - oncrpc cmd 447 retrieves the MAC on i_vzw (but not i_atnt or i_skt).
+ *     Note the octets in the returned value are reversed.
+ */
 static int set_bt_mac(void)
 {
 	struct stat st;
 	FILE *fd;
-	char mac[PROPERTY_VALUE_MAX];
+	char machex[PROPERTY_VALUE_MAX];
+	unsigned char macbuf[8];
 
 	if (stat(BT_ADDR_FILE, &st) == 0)
 	        return 0;
 
-	property_get("service.brcm.bt.mac",mac,"010203040506");
+	property_get("service.brcm.bt.mac", machex, "");
+	if (!machex[0]) {
+		property_get("persist.service.brcm.bt.mac", machex, "");
+	}
+	if (!machex[0]) {
+	        memset(macbuf, 0, sizeof(macbuf));
+	        nv_cmd_remote(0, 447, &macbuf);
+	        if (is_macbuf_valid(macbuf)) {
+	                sprintf(machex, "%02x%02x%02x%02x%02x%02x",
+	                        macbuf[5], macbuf[4], macbuf[3],
+	                        macbuf[2], macbuf[1], macbuf[0]);
+	        }
+	}
+	if (!machex[0])
+	        return -1;
+
 	fd = fopen(BT_ADDR_FILE, "w");
 	fprintf(fd, "%c%c:%c%c:%c%c:%c%c:%c%c:%c%c\n",
-	        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5],
-	        mac[6], mac[7], mac[8], mac[9], mac[10], mac[11]);
+	        machex[0], machex[1], machex[2], machex[3],
+	        machex[4], machex[5], machex[6], machex[7],
+	        machex[8], machex[9], machex[10], machex[11]);
 	fclose(fd);
 
 	return 0;
@@ -54,30 +93,34 @@ static int set_wifi_mac(void)
 {
 	struct stat st;
 	FILE *fd;
-	unsigned char mac[8];
+	unsigned char macbuf[8];
 
 	if (stat(WIFI_ADDR_FILE, &st) == 0)
 	        return 0;
 
-	memset(mac, 0, sizeof(mac)*sizeof(unsigned char));
-	oncrpc_init();
-	oncrpc_task_start();
-	nv_cmd_remote(0, 0x1246, &mac);
+	memset(macbuf, 0, sizeof(macbuf)*sizeof(unsigned char));
+	nv_cmd_remote(0, 0x1246, &macbuf);
 
-	if (mac[0] == 0 && mac[1] == 0)
+	if (!is_macbuf_valid(macbuf))
 		return -1;
 
 	fd = fopen(WIFI_ADDR_FILE, "w");
 	fprintf(fd,"cur_etheraddr=%02x:%02x:%02x:%02x:%02x:%02x\n",
-		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+		macbuf[0], macbuf[1], macbuf[2],
+		macbuf[3], macbuf[4], macbuf[5]);
 	fclose(fd);
 
 	return 0;
 }
 
 /* Read properties and set MAC addresses accordingly */
-int main() {
+int main()
+{
+	oncrpc_init();
+	oncrpc_task_start();
+
         set_bt_mac();
         set_wifi_mac();
+
 	return 0;
 }
